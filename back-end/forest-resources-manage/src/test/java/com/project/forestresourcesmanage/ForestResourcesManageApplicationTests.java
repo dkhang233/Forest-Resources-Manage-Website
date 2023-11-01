e94951257b4e4d6b53704cdfcc15ad2fbcc07aca
Dkhang <caodanhkhang23@gmail.com>
2023-11-01 21:45:41 +0700

    Set up project

---
+++ back-end/forest-resources-manage/src/test/java/com/project/forestresourcesmanage/ForestResourcesManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.forestresourcesmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForestResourcesManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
