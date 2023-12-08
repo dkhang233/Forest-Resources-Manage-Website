4237467ca0d82d82d3e4cbc93b5d94622a8b3d73
Dkhang <caodanhkhang23@gmail.com>
2023-12-08 22:21:13 +0700

    update front-end : animal

--- front-end/forest-resources-manage-application/src/components/animal/AnimalFacility.vue
+++ front-end/forest-resources-manage-application/src/components/animal/AnimalFacility.vue
@@ -4,21 +4,46 @@
             sở</span>
         <stacked-area-chart class="col-start-3 col-span-7  h-[500px] mt-5" :chart-label="chartLabel" :chart-data="chartData"
             v-if="chartData.size != 0"></stacked-area-chart>
-        <div class="mt-5 col-start-3  col-span-2">
-            <span>Ngày bắt đầu</span>
-            <VueDatePicker v-model="beginMonth" month-picker  />
+        <el-select v-model="dataType" class="mt-[3rem] mr-3 col-start-3  col-span-1" placeholder="Select" size="large">
+            <el-option v-for="item in dataTypes" :key="item.label" :label="item.label" :value="item.value" />
+        </el-select>
+        <div class="mt-6 col-start-4  col-span-4 grid grid-cols-2 gap-3" v-if="dataType == 'month'">
+            <div>
+                <span>Bắt đầu</span>
+                <VueDatePicker v-model="beginMonth" month-picker locale="vi" cancelText="Hủy" selectText="Chọn" />
+            </div>
+            <div class="">
+                <span>Kết thúc</span>
+                <VueDatePicker v-model="endMonth" month-picker locale="vi" cancelText="Hủy" selectText="Chọn" />
+            </div>
         </div>
-        <div class="mt-5 col-start-6  col-span-2">
-            <span>Ngày kết thúc</span>
-            <VueDatePicker v-model="endMonth" month-picker/>
+        <div class="mt-6 col-start-4  col-span-4 grid grid-cols-2 gap-3" v-if="dataType == 'quarter'">
+            <div>
+                <span>Bắt đầu</span>
+                <VueDatePicker v-model="beginQuarter" quarter-picker />
+            </div>
+            <div class="">
+                <span>Kết thúc</span>
+                <VueDatePicker v-model="endQuarter" quarter-picker locale="vi" cancelText="Hủy" selectText="Chọn" />
+            </div>
         </div>
-        <VueDatePicker class="mt-5 col-start-3  col-span-2" v-model="endQuarter" quarter-picker />
+        <div class="mt-6 col-start-4  col-span-4 grid grid-cols-2 gap-3" v-if="dataType == 'year'">
+            <div>
+                <span>Bắt đầu</span>
+                <VueDatePicker v-model="beginYear" year-picker locale="vi" cancelText="Hủy" selectText="Chọn" />
+            </div>
+            <div class="">
+                <span>Kết thúc</span>
+                <VueDatePicker v-model="endYear" year-picker locale="vi" cancelText="Hủy" selectText="Chọn" />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import StackedAreaChart from '../chart/StackedAreaChart.vue';
 import *  as animalApi from '@/api/animal';
+import { format ,startOfQuarter } from "date-fns"
 export default {
     name: "animalFacility",
     components: {
@@ -26,19 +51,38 @@ export default {
     },
     data() {
         return {
+            quarter: startOfQuarter(new Date()),
             beginMonth: {
-                month: new Date().getMonth(),
+                month: new Date().getMonth() - 6,
                 year: new Date().getFullYear()
             },
             endMonth: {
-                month: new Date().getMonth(),
+                month: new Date().getMonth() - 1,
                 year: new Date().getFullYear()
             },
-            endQuarter: '',
+            beginQuarter: new Date(),
+            endQuarter: new Date(),
+            beginYear: new Date().getFullYear() - 1,
+            endYear: new Date().getFullYear(),
             chartLabel: [],
             chartData: new Map(),
             chartLabelCopy: [],
-            chartDataCopy: new Map()
+            chartDataCopy: new Map(),
+            dataTypes: [
+                {
+                    value: 'month',
+                    label: 'Tháng',
+                },
+                {
+                    value: 'quarter',
+                    label: 'Quý',
+                },
+                {
+                    value: 'year',
+                    label: 'Năm',
+                }
+            ],
+            dataType: 'month'
         }
     },
     computed: {
@@ -55,19 +99,31 @@ export default {
                 let endMonth = this.endMonth.year + '-' + month + '-01'
                 return endMonth
             }
-        }
+        },
+        formatBeginQuarter() {
+            if (this.beginQuarter != null) {
+                let month = this.beginQuarter.getMonth() < 9 ? "0" + (this.beginQuarter.getMonth() + 1) : (this.beginQuarter.getMonth() + 1)
+                let beginQuarter = this.beginQuarter.getFullYear() + '-' + month + '-01'
+                return beginQuarter
+            }
+        },
+        formatEndQuarter() {
+            if (this.endQuarter != null) {
+                let month = this.endQuarter.getMonth() < 9 ? "0" + (this.endQuarter.getMonth() + 1) : (this.endQuarter.getMonth() + 1)
+                let endQuarter = this.endQuarter.getFullYear() + '-' + month + '-01'
+                return endQuarter
+            }
+        },
+
     },
     watch: {
-        endQuarter(newEndQuarter) {
-            console.log(newEndQuarter)
-        },
         formatBeginMonth(newBeginMonth) {
             if (newBeginMonth != null) {
                 this.chartLabelCopy = this.chartLabel
                 this.chartLabel = []
                 this.chartDataCopy = this.chartData
                 this.chartData.clear()
-                this.setupAnimalQuantityData(newBeginMonth,this.formatEndMonth)
+                this.setupAnimalQuantityData(newBeginMonth, this.formatEndMonth)
             }
         },
         formatEndMonth(newEndMoth) {
@@ -76,13 +132,25 @@ export default {
                 this.chartLabel = []
                 this.chartDataCopy = this.chartData
                 this.chartData.clear()
-                this.setupAnimalQuantityData(this.formatBeginMonth,newEndMoth)
+                this.setupAnimalQuantityData(this.formatBeginMonth, newEndMoth)
             }
+        },
+        formatBeginQuarter(newBeginQuarter) {
+            console.log(newBeginQuarter)
+        },
+        formatEndQuarter(newEndQuarter) {
+            console.log(newEndQuarter)
+        },
+        beginYear(newBeginYear) {
+            console.log(newBeginYear)
+        },
+        endYear(newEndYear) {
+            console.log(newEndYear)
         }
     },
     methods: {
-        setupAnimalQuantityData(beginMonth,endMonth) {
-            animalApi.retrieveAnimalQuantityInMoth(beginMonth,endMonth)
+        setupAnimalQuantityData(beginMonth, endMonth) {
+            animalApi.retrieveAnimalQuantityInMoth(beginMonth, endMonth)
                 .then((res) => {
                     for (let i = res.data.length - 1; i >= 0; i--) {
                         let label = res.data[i].date.slice(0, 7)
@@ -113,10 +181,14 @@ export default {
                         type: 'error',
                     })
                 })
+        },
+        formatQuarter(quarter) {
+            console.log(quarter)
+            return format(quarter, 'QQQ')
         }
     },
     created() {
-        this.setupAnimalQuantityData(this.formatBeginMonth,this.formatEndMonth)
+        this.setupAnimalQuantityData(this.formatBeginMonth, this.formatEndMonth)
     }
 }
 </script>
